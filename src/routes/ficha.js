169b8451995b7f88c2a2a7f90cd85892ca74b381
MuriloLeon <murilo.leon@sptech.school>
2026-05-20 19:32:07 -0300

    Cards das fichas aparecem

--- src/routes/ficha.js
+++ src/routes/ficha.js
@@ -7,7 +7,7 @@ router.post("/criarFicha", function (req, res) {
     fichaController.criarFicha(req, res);
 });
 
-router.get("/buscarFicha:id", function (req, res) {
+router.get("/buscarFicha/:id", function (req, res) {
     console.log('chegou no router')
 
     fichaController.buscarFicha(req, res);
